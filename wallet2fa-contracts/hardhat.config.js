63397932253aac5ae6771d581c93fe068245fdb6
MithilHM <mithilhm@gmail.com>
2025-10-25 17:47:15 +0530

    Initial commit

---
+++ wallet2fa-contracts/hardhat.config.js
@@ -0,0 +1,31 @@
+﻿import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-verify';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+const config = {
+  solidity: {
+    version: '0.8.20',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL || '',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
+  },
+  sourcify: {
+    enabled: false,
+  },
+};
+
+export default config;
